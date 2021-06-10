855aa72af172ebd61f46f8c6b567836d993a3924
sandipan898 <sandipan.das898@gmail.com>
2021-06-11 03:01:11 +0530

    quest 20 using python

--- python/20_check_prime.py
+++ python/20_check_prime.py
@@ -1,4 +1,12 @@
 def is_prime(number):
-    for i in range(2, number/2):
-        return number % i == 0
-        
+    for i in range(2, int(number/2)):
+        if number % i == 0:
+            return False
+    return True
+
+# num = 21
+num = 59
+if is_prime(num):
+    print("{} is a Prime Number".format(num))
+else:
+    print("{} is not a Prime Number".format(num))
